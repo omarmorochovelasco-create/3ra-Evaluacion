2bed13624ea51cc399278327ff491bd5c712f3e2
duransamuel544-rgb <duransamuel544@gmail.com>
2025-12-10 18:50:50 -0400

    Update Tercera Prueba.c

--- Tercera Prueba.c
+++ Tercera Prueba.c
@@ -1,6 +1,7 @@
 // [Omar Velasco C.I: 30.452.558]
 // [Wilmer Oropeza C.I: 31.730.803]
 // [Jhonder Aragot C.I: 33.428.926]
-
+//  [Geremi Duran C.I: 32.860.444]
 // [Sergio Medina C.I : 32.053.351] 
+
 
